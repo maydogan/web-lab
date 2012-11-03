87f0e8fc6b44f6c857e1daa81e196cd306204752
maydogan <meltem.aydogan@bil.omu.edu.tr>
2012-11-03 20:44:00 +0200

    add mysql

---
+++ excel/mysql/homework.sql
@@ -0,0 +1,112 @@
+CREATE TABLE homework (
+  `id` mediumint(8) unsigned NOT NULL auto_increment, 
+  `first_name` varchar(255) default NULL,
+  `last_name` varchar(255) default NULL,
+  `email` varchar(255) default NULL,
+  `address` varchar(255) default NULL,
+  `city` varchar(50) default NULL,
+  `country` varchar(50) default NULL,
+  PRIMARY KEY (`id`)
+) TYPE=MyISAM AUTO_INCREMENT=1; 
+
+
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Glenna','Patton','feugiat@sitametfaucibus.edu','Ap #975-3599 Velit Av.','Ypsilanti','Nicaragua');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Petra','Madden','dolor@aliquetmolestie.org','985-9943 Rutrum St.','Fontana','Aruba');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Philip','Hicks','Cras.pellentesque.Sed@hendrerit.edu','P.O. Box 514, 2790 Eros. Road','Santa Fe Springs','Italy');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Madonna','Russo','dapibus@Pellentesqueultricies.edu','Ap #936-8534 Consectetuer Av.','Salisbury','Turkmenistan');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Shafira','Bond','suscipit.est.ac@senectuset.org','7544 Sapien. Av.','Dayton','Zambia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Colby','Kemp','natoque.penatibus.et@lobortis.org','P.O. Box 557, 8716 Nec Avenue','Torrington','Antigua and Barbuda');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Rhoda','Randolph','Nunc.quis.arcu@ipsumCurabitur.org','Ap #915-443 Amet Road','Glen Cove','Palau');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ray','Malone','libero.dui@magna.org','700-452 A, St.','Jenks','Chad');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Theodore','Garner','arcu@dolor.com','P.O. Box 884, 3348 Cum Avenue','New Rochelle','Saudi Arabia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Grant','Welch','ipsum.leo@erosNam.edu','7419 Ut Rd.','Olean','Cameroon');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Hiroko','Osborn','id.ante@nequeet.ca','7272 Nec Avenue','Oro Valley','Saint Kitts and Nevis');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ruth','Livingston','tempor@diam.com','8900 Phasellus Rd.','Rancho Palos Verdes','Lebanon');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Jenna','Lee','semper@venenatisvelfaucibus.ca','263-119 Quam St.','Pulaski','Central African Republic');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ruth','Huber','arcu.Vestibulum.ut@Nulla.org','P.O. Box 204, 4714 Pharetra. Ave','Oro Valley','Antarctica');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ginger','Hays','elit.a@congueInscelerisque.ca','8854 Cursus, Road','Monterey','Cook Islands');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Armand','Mccarty','eu.dolor.egestas@Nullamnisl.edu','652-3858 Mi. Rd.','Chattanooga','Paraguay');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Josephine','Meyer','enim.Etiam@tellusidnunc.edu','7711 Elit Ave','Frankfort','Cameroon');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kirsten','Conley','interdum.feugiat.Sed@mi.org','P.O. Box 621, 6906 Tellus. Ave','Rock Island','Argentina');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Emmanuel','Camacho','enim@veliteu.com','P.O. Box 284, 6054 Gravida. Av.','Saukville','Yemen');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Gwendolyn','Burnett','lectus@Nulla.org','165-9060 Non St.','Latrobe','Niue');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Leslie','Dillon','et.euismod.et@egetmollis.ca','P.O. Box 252, 5030 Nec Ave','Hackensack','Guyana');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Berk','Christian','aliquam.enim@et.com','P.O. Box 263, 5719 Donec Road','Lancaster','Egypt');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Jena','Whitfield','senectus.et.netus@est.ca','5982 Hendrerit Rd.','Columbus','Belize');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Quamar','Oneal','vitae.nibh@augueeutellus.org','P.O. Box 686, 5300 Id Avenue','Hidden Hills','Russian Federation');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Nina','Weber','pharetra@mauriselit.ca','505-9726 Ac Road','West Covina','France');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Lacota','Cantrell','montes.nascetur@aliquamiaculislacus.com','8702 Bibendum Rd.','Helena','Virgin Islands, U.S.');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kasper','Travis','risus.quis@enimcondimentum.com','Ap #498-2392 Non Ave','Attleboro','Turkey');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Mona','Alvarez','Morbi.neque.tellus@consectetueradipiscing.com','P.O. Box 441, 4493 Accumsan Av.','Oro Valley','Grenada');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Faith','Barton','metus@atortorNunc.edu','Ap #224-7441 Magna. St.','Danbury','Monaco');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Xyla','Duran','neque.venenatis@metus.org','P.O. Box 789, 1969 Cursus Road','Covina','Burundi');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Malcolm','Stein','nibh.dolor@utipsumac.ca','4409 Amet Rd.','Long Beach','Israel');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Lara','Downs','enim.Mauris@id.org','P.O. Box 560, 467 Phasellus St.','Guthrie','Luxembourg');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Richard','Lynch','feugiat.non.lobortis@Praesent.com','915-3844 Vivamus Rd.','Saginaw','Sao Tome and Principe');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Rooney','Faulkner','Nam@Aliquamfringilla.org','P.O. Box 668, 9453 Mauris Ave','Compton','Burkina Faso');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Yoshio','Macdonald','consectetuer.adipiscing@Mauris.org','P.O. Box 469, 4735 Sapien Avenue','Kearney','Gabon');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Shaine','Cummings','pede@iaculisenim.edu','Ap #801-9127 Convallis St.','Salem','Indonesia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Sierra','Wilkerson','iaculis.lacus@Aeneaneuismod.org','916-5489 Phasellus Rd.','Marshall','Gambia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Adrian','Wilson','sem.egestas@at.org','Ap #743-1761 Aliquam St.','Chelsea','Liberia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Austin','Waller','porttitor.vulputate.posuere@convallisantelectus.ca','149-2640 Dictum Ave','Rock Island','Venezuela');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Rhona','Fleming','In@ornare.org','P.O. Box 238, 6601 Sodales Rd.','Rockford','Tajikistan');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Sacha','Valencia','sem.semper@quamvelsapien.ca','Ap #270-4117 Diam. Ave','Aliquippa','Dominica');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Melinda','Riggs','convallis@sagittis.com','8005 Eu Road','Atlantic City','Burkina Faso');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Whilemina','Padilla','ipsum.ac.mi@atpede.edu','340-4301 Quis Rd.','Columbus','Timor-leste');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Lyle','Mcgee','neque.sed.sem@lacus.edu','Ap #997-736 Turpis. Ave','West Palm Beach','United States Minor Outlying Islands');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Slade','Abbott','Cras@sitametconsectetuer.org','6526 Fusce Av.','Laguna Beach','Ethiopia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Tyrone','Taylor','mi.lacinia@quisdiam.org','490-486 Tristique Street','Boise','Yemen');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Allistair','Cervantes','euismod.in.dolor@lobortis.edu','P.O. Box 527, 8711 Iaculis, Rd.','Steubenville','France');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kasimir','George','in.consequat@a.edu','1120 Vel St.','Jacksonville','France');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kelsie','Hurley','vel.nisl@maurisblandit.com','598-4200 Suspendisse Rd.','Beckley','Nepal');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Tamara','Walter','sem.Nulla@nuncIn.ca','1392 At, St.','Salem','Turkey');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ariana','Strong','viverra@duiin.ca','475-7751 Leo. Street','Temple City','Sudan');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Flynn','Foley','Nunc.sollicitudin.commodo@netus.com','P.O. Box 222, 2104 Vivamus Street','Eufaula','United Kingdom');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Fuller','Beard','aliquet.Proin.velit@dictumcursusNunc.ca','Ap #367-2707 Metus. Avenue','Seal Beach','Portugal');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Allistair','Pate','iaculis.aliquet@convallis.com','Ap #610-6218 Ipsum. Street','West Memphis','Philippines');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Russell','Delaney','eros.turpis@tempor.org','620-6454 Arcu. Ave','Seaford','Tanzania, United Republic of');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Thane','Morgan','sollicitudin.commodo@rhoncusid.com','Ap #591-9139 Morbi Rd.','Gettysburg','Bosnia and Herzegovina');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Keiko','Fitzgerald','Aliquam.rutrum.lorem@apurus.org','P.O. Box 133, 6215 Nunc. Avenue','Half Moon Bay','Nepal');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Denton','Coffey','sem.molestie@cursusIntegermollis.edu','575-8242 Phasellus St.','Ames','Bangladesh');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Oscar','Gonzalez','per@ametconsectetuer.org','P.O. Box 980, 4144 Blandit Rd.','Clemson','American Samoa');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Alexander','Long','orci.tincidunt@Namconsequatdolor.com','Ap #754-4240 Mauris St.','Wheaton','Egypt');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Daquan','Wood','nec@penatibuset.edu','1521 Morbi Rd.','Wisconsin Dells','Western Sahara');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Brent','Harrell','vitae@euismodest.ca','1616 Lacinia Road','Woburn','Belgium');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Noah','Paul','sociis.natoque@egestaslaciniaSed.org','8190 Eu Ave','Westminster','Virgin Islands, U.S.');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Galvin','Schwartz','sollicitudin@Proinnislsem.org','P.O. Box 711, 1568 Aliquet Road','Shelton','Bosnia and Herzegovina');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Alma','Wolfe','fringilla@eratnonummyultricies.org','129-814 Vivamus Ave','Boston','Antarctica');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Tamekah','Benson','in@ametanteVivamus.org','706-7516 In Road','High Point','Guadeloupe');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Tashya','Curry','felis.adipiscing@augue.edu','Ap #336-1452 Et, Road','Battle Creek','Finland');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Clinton','Cervantes','amet@arcu.com','8112 Cras Rd.','Shelton','Brunei Darussalam');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Jillian','Mccall','elementum.lorem@velpede.com','4746 Eleifend. Avenue','Shelton','Croatia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Camille','Bradley','Mauris.non@Duisdignissim.com','Ap #145-9901 Pharetra. Rd.','Pocatello','Luxembourg');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Solomon','Langley','interdum.libero@gravida.org','223-8110 Lacus Road','Concord','Zambia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Duncan','Atkinson','in@hendrerit.org','P.O. Box 299, 8029 Eu, St.','Nichols Hills','Estonia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Reece','Gilmore','venenatis.lacus.Etiam@fringillaDonecfeugiat.org','P.O. Box 732, 4489 In, St.','Jeffersontown','Suriname');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Debra','Merrill','arcu.Sed.eu@orcilobortis.ca','Ap #337-7605 Proin Rd.','Ogden','Lebanon');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kyla','Fields','dignissim.lacus.Aliquam@Sednulla.edu','Ap #418-7123 Donec Rd.','Sherrill','Austria');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Cameran','Durham','urna.justo.faucibus@ultricesposuerecubilia.ca','2897 Proin Av.','Hudson','Madagascar');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Kibo','Chambers','tincidunt@duiquisaccumsan.ca','168-5809 Fusce St.','Yorba Linda','Russian Federation');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Vaughan','Parker','ipsum.dolor@maurisipsumporta.ca','Ap #278-2399 Sociosqu Ave','Hope','Bermuda');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Karina','Rowe','diam.Pellentesque@amifringilla.edu','7511 Magnis Street','Grand Island','Costa Rica');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Sacha','Vazquez','id@enimCurabiturmassa.edu','7264 Augue, Ave','Apple Valley','Norway');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Gannon','David','tincidunt.Donec.vitae@eleifend.org','773-5724 Quis, Rd.','City of Industry','Albania');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Jacob','Vasquez','fringilla.ornare@tincidunttempusrisus.ca','4386 Tellus. St.','Wichita','Marshall Islands');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Hayden','Owen','libero.mauris@lacinia.ca','372-842 Tristique St.','Signal Hill','Sierra Leone');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ryder','Heath','elit.a.feugiat@lectuspedeet.edu','455-9388 Ridiculus Rd.','Mount Pleasant','Chad');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Hadassah','Marks','diam.Proin.dolor@Morbi.ca','671 Ornare, St.','Del Rio','Brazil');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Grace','Foreman','enim.Mauris.quis@quis.edu','P.O. Box 224, 7485 Euismod Street','North Tonawanda','Mozambique');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Cade','Mcclain','eget@vulputateduinec.edu','574-3046 Arcu St.','Kenner','Fiji');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ginger','Noel','vel.quam@Maecenasornareegestas.ca','642 Aliquam Ave','Wichita Falls','Albania');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Chester','Perez','dui.in@luctusetultrices.ca','Ap #299-8984 Ante St.','Reedsport','Mayotte');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Iliana','Barr','dignissim@magnaa.edu','Ap #984-7623 Dictum. Rd.','El Cerrito','Virgin Islands, U.S.');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Rosalyn','Deleon','aliquam.adipiscing.lacus@velvenenatisvel.ca','9886 Mauris Av.','Atwater','Macedonia');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Clio','Walters','tortor@Quisqueporttitor.ca','293-8134 Arcu. Ave','Cape Girardeau','Romania');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Regan','Taylor','tristique.aliquet.Phasellus@primisinfaucibus.ca','566-9916 Ac St.','Dover','French Guiana');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Yolanda','Ferguson','Phasellus.in.felis@odio.ca','6718 Magna. Road','Billings','Czech Republic');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Sylvester','Thornton','ante.bibendum@risusquisdiam.org','2213 Est, Avenue','Pascagoula','American Samoa');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Leslie','Cooper','tellus@Loremipsum.edu','469-1509 Penatibus St.','Vancouver','Singapore');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Tashya','Mcdonald','metus.sit.amet@adipiscingelit.com','Ap #725-9430 Eget Rd.','Port Arthur','Malawi');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Camilla','Harvey','sed.consequat@sedlibero.org','Ap #486-7150 Erat. Rd.','Ansonia','Suriname');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Shellie','Watkins','vulputate.dui@Proinvel.ca','191-7913 At Street','Brookings','Sierra Leone');
+INSERT INTO `homework` (`first_name`,`last_name`,`email`,`address`,`city`,`country`) VALUES ('Ori','Scott','pede@volutpat.org','P.O. Box 932, 9551 Egestas. Road','Tuscaloosa','Guadeloupe');
